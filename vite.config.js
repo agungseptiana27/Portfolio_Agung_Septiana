6a9cc75ca05bae7528d5919664df917a6f3fa5a2
agungseptiana27 <agungseptiana02@gmail.com>
2025-02-26 11:43:02 +0700

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Pastikan hasil build ada di folder "build"
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "three"],
+        },
+      },
+    },
   },
 });
